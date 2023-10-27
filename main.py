9a814f4ead4a8d9de056504ddf75c36620a3b731
isabellaarmendariz <iarmendariz@ufl.edu>
2023-10-27 18:19:21 -0400

    full code

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
-# Sydney Vargo &
+# Sydney Vargo & Isabella Armendariz
 # October 25, 2023
 
 def main():
     # make menu
-    print("Menu")
-    print("_____________")
+    print("\nMenu")
+    print("-------------")
     print("1. Encode")
     print("2. Decode")
     print("3. Quit")
-
-    # get choice
-    choice = int(input("Please choose an option: "))
-
-    # if choice == 1
-    if choice == 1:
-        num = input("Please enter the number you would like encoded: ")
-        answer = encode(num)
-        print(answer)
 
 
 def encode(num):
@@ -54,5 +45,50 @@ def encode(num):
     return answer
 
 
+def decode(encodepass):
+    # create lists
+    num_list = []
+    num_list2 = []
+
+    # add digits in encoded password to list
+    for char in encodepass:
+        num_list.append(char)
+
+    # make the characters in the list into integers
+    num_list = [int(char) for char in num_list]
+
+    # decrease by 3 and account for exceptions
+    for digit in num_list:
+        if digit == 2:
+            new_digit = 9
+        elif digit == 1:
+            new_digit = 8
+        elif digit == 0:
+            new_digit = 7
+        else:
+            new_digit = digit - 3
+
+        # add changed digits to a new list
+        num_list2.append(new_digit)
+
+    # make the password a string again
+    decodepass = ""
+    for value in num_list2:
+        decodepass += str(value)
+
+    # return string
+    return decodepass
+
+
 if __name__ == '__main__':
-    main()
+    while True:
+        main()
+        choice = int(input("Please enter an option: "))
+        if choice == 3:
+            break
+        elif choice == 1:
+            num = input("Please enter your password to encode: ")
+            encodepass = encode(num)
+            print("Your password has been encoded and stored!")
+        elif choice == 2:
+                print(f"The encoded password is {encode(num)}, and the original password is {decode(encodepass)}.")
